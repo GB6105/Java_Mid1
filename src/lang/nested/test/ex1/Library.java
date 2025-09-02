58d233b41fe44ef533c4e7aa307231ce63f70458
GB <xorbskim6105@gmail.com>
2025-09-03 08:47:35 +0900

    feat/8_문제 5 풀이

--- src/lang/nested/test/ex1/Library.java
+++ src/lang/nested/test/ex1/Library.java
@@ -1,4 +1,35 @@
 package lang.nested.test.ex1;
 
 public class Library {
+    Book[] books;
+    int bookCount = 0;
+
+    public Library(int count){
+        this.books = new Book[count];
+    }
+
+    private class Book{
+        private String title;
+        private String author;
+
+        Book(String title, String author){
+            this.title = title;
+            this.author = author;
+        }
+    }
+
+    public void addBook(String title, String author){
+        if(bookCount >= books.length){
+            System.out.println("도서관 저장 공간이 부족합니다.");
+        }
+        books[bookCount] = new Book(title, author);
+        bookCount++;
+    }
+
+    public void showBooks(){
+        System.out.println("== 책 목록 출력==");
+        for(Book book : books){
+            System.out.println("도서 제목: " + book.title + ", 저자:" + book.author);
+        }
+    }
 }
