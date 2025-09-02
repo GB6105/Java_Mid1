f4d2c7102e1aff0e4b6092ba69edd8a1fbb389ed
GB <xorbskim6105@gmail.com>
2025-09-03 08:53:44 +0900

    feat/8_문제 5 해설 참조

--- src/lang/nested/test/ex1/Library.java
+++ src/lang/nested/test/ex1/Library.java
@@ -1,18 +1,23 @@
 package lang.nested.test.ex1;
 
 public class Library {
+    // private으로 선언
     private Book[] books;
-    private int bookCount = 0;
+    private int bookCount;
 
     public Library(int count){
-        this.books = new Book[count];
+        // 생성자에서는 굳이 this 안써줘도 됨
+        books = new Book[count];
+        // 여기서 0으로 초기화
+        bookCount = 0;
     }
 
-    private class Book{
+    //이 클래스 안에서 쓰지만 전혀 다른 객체임(인스턴스 공유 안함)
+    private static class Book{
         private String title;
         private String author;
 
-        Book(String title, String author){
+        public Book(String title, String author){
             this.title = title;
             this.author = author;
         }
@@ -21,7 +26,7 @@ public class Library {
     public void addBook(String title, String author){
         if(bookCount >= books.length){
             System.out.println("도서관 저장 공간이 부족합니다.");
-        }else{
+        }else{// else 문으로 꼭 감싸주어야함
             books[bookCount] = new Book(title, author);
             bookCount++;
         }
