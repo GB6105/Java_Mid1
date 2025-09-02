70ebdedb262e6cee644380e96aae735445e3a714
Tae Kyoun Kim <xorbskim6105@gmail.com>
2025-09-02 16:51:46 +0900

    feat/6_문제 3 풀이

---
+++ src/lang/time/test/TestBetween.java
@@ -0,0 +1,18 @@
+package lang.time.test;
+
+import javax.lang.model.SourceVersion;
+import java.time.LocalDate;
+import java.time.Period;
+
+public class TestBetween {
+    public static void main(String[] args) {
+        LocalDate startDate = LocalDate.of(2024,1,1);
+        LocalDate endDate = LocalDate.of(2024,11,21);
+        Period between = Period.between(startDate, endDate);
+
+        System.out.println("시작 날짜: " + startDate);
+        System.out.println("목표 날짜: " + endDate);
+        System.out.println("남은 기간: " + between.getYears() + "년 " + between.getMonths() + "개월 " + between.getDays() + "일 ");
+        System.out.println("디데이: " + between.toTotalMonths());
+    }
+}
--- src/lang/time/test/TestLoopPlus.java
+++ src/lang/time/test/TestLoopPlus.java
@@ -3,15 +3,21 @@ package lang.time.test;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 public class TestLoopPlus {
     public static void main(String[] args) {
-        LocalDate dateTime = LocalDate.of(2024,1,1);
-        Period period = Period.ofWeeks(2);
-        for(int i = 1; i <= 5; i ++){
-//            LocalDate nextWeek = dateTime.plus(period);
-            System.out.println("날짜" + i +": " + dateTime);
-            dateTime = dateTime.plus(period);
+//        LocalDate dateTime = LocalDate.of(2024,1,1);
+//        Period period = Period.ofWeeks(2);
+//        for(int i = 1; i <= 5; i ++){
+//            System.out.println("날짜" + i +": " + dateTime);
+//            dateTime = dateTime.plus(period);
+//        }
+        //누적 합이 아닌 반복 횟수 와 chronoUnit의 사용
+        LocalDate startDate = LocalDate.of(2024, 1, 1);
+        for(int i = 0 ; i < 5; i++){
+            LocalDate nextDate = startDate.plus(2 * i, ChronoUnit.WEEKS);
+            System.out.println("날짜 " + (i+1) + " : " + nextDate);
         }
     }
 }
