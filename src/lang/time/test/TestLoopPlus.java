58e65182174e36032994b6d12aae22b54ff1051c
Tae Kyoun Kim <xorbskim6105@gmail.com>
2025-09-02 17:18:16 +0900

    feat/6_문제 3 편의 메서드

--- src/lang/time/test/TestLoopPlus.java
+++ src/lang/time/test/TestLoopPlus.java
@@ -16,7 +16,8 @@ public class TestLoopPlus {
         //누적 합이 아닌 반복 횟수 와 chronoUnit의 사용
         LocalDate startDate = LocalDate.of(2024, 1, 1);
         for(int i = 0 ; i < 5; i++){
-            LocalDate nextDate = startDate.plus(2 * i, ChronoUnit.WEEKS);
+            //LocalDate nextDate = startDate.plus(2 * i, ChronoUnit.WEEKS);
+            LocalDate nextDate = startDate.plusWeeks(2 * i);
             System.out.println("날짜 " + (i+1) + " : " + nextDate);
         }
     }
--- src/lang/time/test/TestZone.java
+++ src/lang/time/test/TestZone.java
@@ -1,19 +1,19 @@
 package lang.time.test;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 
 public class TestZone {
     public static void main(String[] args) {
-        //서울
-        LocalDateTime seoulLdt = LocalDateTime.of(2024,1,1,9,0);
-        //런던
-        ZonedDateTime londonLdt = ZonedDateTime.of(seoulLdt, ZoneId.of("Europe/London"));
-        //뉴욕
-        ZonedDateTime newYorkLdt = ZonedDateTime.of(seoulLdt, ZoneId.of("America/New_York"));
+//        //서울
+//        LocalDateTime seoulLdt = LocalDateTime.of(2024,1,1,9,0);
+//        //런던
+//        ZonedDateTime londonLdt = ZonedDateTime.of(seoulLdt, ZoneId.of("Europe/London"));
+//        //뉴욕
+//        ZonedDateTime newYorkLdt = ZonedDateTime.of(seoulLdt, ZoneId.of("America/New_York"));
 
+        ZonedDateTime seoulLdt = ZonedDateTime.of(LocalDate.of(2024,1,1), LocalTime.of(9,0),ZoneId.of("Asia/Seoul"));
+        ZonedDateTime londonLdt = seoulLdt.withZoneSameInstant(ZoneId.of("Europe/London"));
+        ZonedDateTime newYorkLdt = seoulLdt.withZoneSameInstant(ZoneId.of("America/New_York"));
         System.out.println("서울의 회의 시간: " + seoulLdt);
         System.out.println("런던의 회의 시간: " + londonLdt);
         System.out.println("뉴욕의 회의 시간: " + newYorkLdt);
