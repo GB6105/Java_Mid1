c19fecf05194b2d6d749ab796bfd6d5e435df976
Tae Kyoun Kim <xorbskim6105@gmail.com>
2025-09-03 15:27:18 +0900

    feat/10_예제 1 & 2 코드

---
+++ src/lang/execption/ex2/NetworkServiceV2_2.java
@@ -0,0 +1,22 @@
+package lang.execption.ex2;
+
+public class NetworkServiceV2_2 {
+    public void sendMessage(String data)  {
+        String address =  "http://example.com";
+        NetworkClientV2 client = new NetworkClientV2(address);
+        client.initError(data);
+
+        try {
+            client.connect();
+        } catch (NetworkClientExceptionV2 e) {
+            System.out.println("[오류] 코드 : " + e.getErrorCode() + ", 메시지 : " + e.getMessage());
+            return; // 커넥트가 안되면 전송을 막음
+        }
+        try {
+            client.send(data);
+        } catch (NetworkClientExceptionV2 e) {
+            System.out.println("[오류] 코드 : " + e.getErrorCode() + ", 메시지 : " + e.getMessage());
+        }
+        client.disconnect();
+    }
+}
