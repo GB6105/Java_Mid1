4d032030528749177b73717e652ec3a1c80f9701
GB <xorbskim6105@gmail.com>
2025-09-03 09:46:27 +0900

    feat/9_예외 처리 예제

---
+++ src/lang/execption/ex0/NetworkClientV0.java
@@ -0,0 +1,25 @@
+package lang.execption.ex0;
+
+import javax.lang.model.SourceVersion;
+
+public class NetworkClientV0 {
+    private final String address;
+
+    public NetworkClientV0(String address) {
+        this.address = address;
+    }
+
+    public String connect(){
+        System.out.println(address + " 서버 연결 성공");
+        return "success";
+    }
+
+    public String send(String data){
+        System.out.println(address + " 서버에 데이터 전송 : " + data);
+        return "success";
+    }
+
+    public void disconnect(){
+        System.out.println(address + " 서버 연결 해제");
+    }
+}
