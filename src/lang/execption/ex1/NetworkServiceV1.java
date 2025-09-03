825bf85557fe179a6874c0f085e7f380d8f21964
GB <xorbskim6105@gmail.com>
2025-09-03 11:45:11 +0900

    feat/9_예제 1 & 2

---
+++ src/lang/execption/ex1/NetworkServiceV1.java
@@ -0,0 +1,14 @@
+package lang.execption.ex1;
+
+
+public class NetworkServiceV1 {
+    public void sendMessage(String data){
+        String address =  "http://example.com";
+        NetworkClientV1 client = new NetworkClientV1(address);
+        client.initError(data);
+
+        client.connect();
+        client.send(data);
+        client.disconnect();
+    }
+}
